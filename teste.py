61f9843b5fd45bfeb5a38c80c9eefcd00a5bc79b
MatheusNevs <matheusnf62@gmail.com>
2023-05-29 22:03:00 -0300

    29/05

--- teste.py
+++ teste.py
@@ -1,5 +1,17 @@
-msg = input()
-for i in msg[::-1]:
-    if i.islower():
-        print(i, end = '')
-print()
+mensagem = input().split('=')
+left = mensagem[0]
+right = mensagem[1]
+try:
+    isoled = int(left)
+    variable = right.split('+')
+except:
+    variable = left.split('+')
+    isoled = int(right)
+
+for c in variable:
+    if c.isnumeric():
+        isoled += -(int(c))
+    else:
+        letter = c
+
+print(f'{letter} = {isoled}')
